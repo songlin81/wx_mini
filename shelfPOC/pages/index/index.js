4e281be3437f8ec23bab2b70e036d82ebe443952
Lin Song <song.lin@volvo.com>
2020-06-24 20:06:10 +0800

    weather page added
    weather page added

--- shelfPOC/pages/index/index.js
+++ shelfPOC/pages/index/index.js
@@ -1,13 +1,10 @@
 Page({
-  onLoad: function(options) {
+  onLoad: function() {
     var that = this;
     wx.request({
       url: 'http://47.104.244.246:5001/getList',
-      header: {
-        'content-type': 'application/json'
-      },
+      header: {'content-type': 'application/json'},
       success: function (res) {
-        //console.log(res.data.bookList)
         that.setData({
             isDownloading: false,
             percentNum: 0,
@@ -17,46 +14,14 @@ Page({
     })
   },
 
-  data: {
-    isDownloading: false,
-    percentNum: 0,
-    bookList: []
-  },
-
-  openBook: function(path) {
-    wx.openDocument({
-      filePath: path,
-      success: function(res) {
-        console.log('打开图书成功')
-      },
-      fail: function(error) {
-        console.log(error);
-      }
-    })
-  },
-
-  saveBook: function(id, path) {
-    var that = this
-    wx.saveFile({
-      tempFilePath: path,
-      success: function(res) {
-        let newPath = res.savedFilePath
-        wx.setStorageSync(id, newPath)
-        that.openBook(newPath)
-      },
-      fail: function(error) {
-        console.log(error)
-        that.showTips('文件保存失败！')
-      }
-    })
-  },
+  data: {},
 
   readBook: function(e) {
     var that = this
     let id = e.currentTarget.dataset.id
     let fileUrl = e.currentTarget.dataset.file
-    console.log(id);
-    console.log(fileUrl);
+    // console.log(id);
+    // console.log(fileUrl);
     let path = wx.getStorageSync(id)
     if (path == '') {
       that.setData({
@@ -96,11 +61,45 @@ Page({
     }
   },
 
+  openBook: function(path) {
+    wx.openDocument({
+      filePath: path,
+      success: function(res) {
+        console.log('打开图书成功')
+      },
+      fail: function(error) {
+        console.log(error);
+      }
+    })
+  },
+
+  saveBook: function(id, path) {
+    var that = this
+    wx.saveFile({
+      tempFilePath: path,
+      success: function(res) {
+        let newPath = res.savedFilePath
+        wx.setStorageSync(id, newPath)
+        that.openBook(newPath)
+      },
+      fail: function(error) {
+        console.log(error)
+        that.showTips('文件保存失败！')
+      }
+    })
+  },
+
   showTips: function(content) {
     wx.showModal({
       title: '提醒',
       content: content,
       showCancel: false
     })
-  }
+  },
+
+  navWeather: function(){
+    wx.navigateTo({
+      url: "/pages/weather/weather?version=v1"
+    });
+  },
 })
---
+++ shelfPOC/pages/weather/weather.js
@@ -0,0 +1,11 @@
+Page({
+  onLoad: function(params) {
+    this.setData({
+      versionNo: params.version
+    })
+  },
+
+  data: {
+    versionNo:''
+  }
+})
