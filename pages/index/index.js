c5f1fe8229b1a921741042f890e031709a399ee8
Lin Song <song.lin@volvo.com>
2020-06-24 11:10:17 +0800

    shelve top region done

---
+++ pages/index/index.js
@@ -0,0 +1,106 @@
+Page({
+  onLoad: function(options) {
+    var that = this;
+    wx.request({
+      url: 'http://47.104.244.246:5001/getList',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data.bookList)
+        that.setData({
+            isDownloading: false,
+            percentNum: 0,
+            bookList: res.data.bookList
+        })
+      }
+    })
+  },
+
+  data: {
+    isDownloading: false,
+    percentNum: 0,
+    bookList: []
+  },
+
+  openBook: function(path) {
+    wx.openDocument({
+      filePath: path,
+      success: function(res) {
+        console.log('打开图书成功')
+      },
+      fail: function(error) {
+        console.log(error);
+      }
+    })
+  },
+
+  saveBook: function(id, path) {
+    var that = this
+    wx.saveFile({
+      tempFilePath: path,
+      success: function(res) {
+        let newPath = res.savedFilePath
+        wx.setStorageSync(id, newPath)
+        that.openBook(newPath)
+      },
+      fail: function(error) {
+        console.log(error)
+        that.showTips('文件保存失败！')
+      }
+    })
+  },
+
+  readBook: function(e) {
+    var that = this
+    let id = e.currentTarget.dataset.id
+    let fileUrl = e.currentTarget.dataset.file
+    console.log(id);
+    console.log(fileUrl);
+    let path = wx.getStorageSync(id)
+    if (path == '') {
+      that.setData({
+        isDownloading: true
+      })
+      const downloadTaskInit = wx.downloadFile({
+        url: fileUrl,
+        success: function(res) {
+          that.setData({
+            isDownloading: false
+          })
+          if (res.statusCode == 200) {
+            path = res.tempFilePath
+            that.saveBook(id, path)
+          }
+          else {
+            that.showTips('暂时无法下载！')
+          }
+        },
+        fail: function(e) {
+          that.setData({
+            isDownloading: false
+          })
+          that.showTips('无法连接到服务器！')
+        }
+      })
+
+      downloadTaskInit.onProgressUpdate(function(res) {
+        let progress = res.progress;
+        that.setData({
+          percentNum: progress
+        })
+      })
+    }
+    else {
+      that.openBook(path)
+    }
+  },
+
+  showTips: function(content) {
+    wx.showModal({
+      title: '提醒',
+      content: content,
+      showCancel: false
+    })
+  }
+})
