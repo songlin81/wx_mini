38b7552f71c00b87e3ed69e9a29e5be1ae813c61
Lin Song <song.lin@volvo.com>
2020-06-25 22:08:42 +0800

    pull to refresh added to news page

--- shelfPOC/pages/news/news.js
+++ shelfPOC/pages/news/news.js
@@ -1,6 +1,8 @@
 var common = require('../../utils/common.js')
 Page({
   data: {
+    hiddenloading: true,
+    inventoryList: []
   },
   onLoad: function(options) {
     var that = this;
@@ -29,6 +31,29 @@ Page({
   },
 
   onReachBottom: function () {
-    console.log('加载更多')
+    var that = this;
+    //wx.showLoading({title: '加载中…', icon: 'loading'})
+    wx.showToast({title: '加载中', icon: 'loading'});
+    this.setData({ hiddenloading:false})
+    common.getSwiperList({
+      method: 'GET',
+      data: {},
+      success: function(res){
+        for(var i=0;i<res.data.data.news.length;i++){
+          that.data.inventoryList.push(res.data.data.news[i])
+        }
+        that.setData({
+          inventoryList: that.data.inventoryList
+        })
+      },
+      fail: function(res){
+        console.log('fail: '+res.errMsg.toString())
+      },
+      complete: function(res){
+        console.log('complete: '+res.statusCode.toString())
+        wx.hideToast();
+        //wx.hideLoading()
+      }
+    })
   },
 })
