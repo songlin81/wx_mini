a7680cdda99d13735c9aba0ca95bfd77bdfb3eec
Lin Song <song.lin@volvo.com>
2020-06-26 06:44:08 +0800

    flex updated

--- shelfPOC/pages/news/news.js
+++ shelfPOC/pages/news/news.js
@@ -2,15 +2,18 @@ var common = require('../../utils/common.js')
 Page({
   data: {
     hiddenloading: true,
-    inventoryList: []
+    inventoryList: [],
+    page: 1,
+    hasmoreData: true,
+    hiddenloading: true
   },
   onLoad: function(options) {
     var that = this;
     common.getSwiperList({
       method: 'GET',
-      data: {},
+      data: {"page": that.data.page},
       success: function(res){
-        console.log(res);
+        //console.log(res);
         that.setData({
           swiperList: res.data.data.slides,
           inventoryList: res.data.data.news
@@ -31,27 +34,31 @@ Page({
   },
 
   onReachBottom: function () {
-    var that = this;
-    //wx.showLoading({title: '加载中…', icon: 'loading'})
-    wx.showToast({title: '加载中', icon: 'loading'});
+    var that = this
+    that.data.page++
     this.setData({ hiddenloading:false})
     common.getSwiperList({
       method: 'GET',
-      data: {},
+      data: {"page": that.data.page},
       success: function(res){
-        for(var i=0;i<res.data.data.news.length;i++){
-          that.data.inventoryList.push(res.data.data.news[i])
+        console.log(res.data.data.news.length)
+        if(res.data.data.news.length>0){
+          console.log('???')
+          for(var i=0;i<res.data.data.news.length;i++){
+            that.data.inventoryList.push(res.data.data.news[i])
+          }
+          that.setData({
+            inventoryList: that.data.inventoryList
+          })
+        }else{
+          console.log('YYYY')
         }
-        that.setData({
-          inventoryList: that.data.inventoryList
-        })
       },
       fail: function(res){
         console.log('fail: '+res.errMsg.toString())
       },
       complete: function(res){
         console.log('complete: '+res.statusCode.toString())
-        wx.hideToast();
         //wx.hideLoading()
       }
     })
